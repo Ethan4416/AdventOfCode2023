085fa25c6e336bbe2f30afddd26243bd34a41c4d
Ethan ULETH <ethan.predinchuk@uleth.ca>
2023-12-04 12:59:31 -0700

    Commiting the start of Day 4

---
+++ Advent Code Day 4.py
@@ -0,0 +1,5 @@
+# textFile = open("C:\\Users\Predinchuk\AdventOfCode2023\Advent Day 4 Inputs.txt")
+textFile = open("C:\\Users\Predinchuk\AdventOfCode2023\Testing Day 4.txt")
+
+for line in textFile:
+    print(textFile)
